9f82f7d0b7b8e9b76db93f365d598a6458ea1859
Sergey Skotnitskiy <sergeyskotnitskiy@gmail.com>
2016-05-13 17:57:44 +0300

    add SerialDTO

---
+++ complete/src/main/java/viapi/dto/SerialDTO.java
@@ -0,0 +1,16 @@
+package viapi.dto;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerialDTO {
+    public String shortTitle;
+    public String fullTitle;
+    public URL serialUrl;
+    public URL imageUrl;
+    public String titleInfoItem;
+    public String titleInfoVotePositive;
+    public String titleInfoVoteNegative;
+    public List<String> qualities = new ArrayList<String>();
+}
