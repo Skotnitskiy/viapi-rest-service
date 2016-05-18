1da9916109f25348b9c7795c58cafffda8ca9f68
Sergey Skotnitskiy <sergeyskotnitskiy@gmail.com>
2016-05-19 01:35:14 +0300

    added compression

--- complete/src/main/java/viapi/controller/VideoTitlesController.java
+++ complete/src/main/java/viapi/controller/VideoTitlesController.java
@@ -2,6 +2,7 @@ package viapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import viapi.dto.VideoTitlesDTO;
@@ -15,7 +16,7 @@ public class VideoTitlesController {
     @Autowired
     IVideoTitlesService videoTitlesService;
 
-    @RequestMapping("/serials")
+    @RequestMapping(value = "/serials", method = RequestMethod.GET)
     public List<VideoTitlesDTO> videoTitles(@RequestParam(name = "page", defaultValue = "0") String pageNumber,
                                             @RequestParam(name = "sort", defaultValue = Sort.TREND) String sort) {
         return videoTitlesService.getVideoTitles(pageNumber, sort);
