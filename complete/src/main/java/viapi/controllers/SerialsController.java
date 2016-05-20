d6cedb6312c22a4c1f8a2632fb22e1076747e76f
Sergey Skotnitskiy <sergeyskotnitskiy@gmail.com>
2016-05-20 14:53:38 +0300

    changed method name

--- complete/src/main/java/viapi/controllers/SerialsController.java
+++ complete/src/main/java/viapi/controllers/SerialsController.java
@@ -16,7 +16,7 @@ public class SerialsController {
     IVideoTitlesService videoTitlesService;
 
     @RequestMapping(value = "/show_start/{year}", method = RequestMethod.GET)
-    public List<VideoTitlesDTO> videoTitlesSerials(@RequestParam(name = "page", defaultValue = "0") String pageNumber,
+    public List<VideoTitlesDTO> groupByVideoTitles(@RequestParam(name = "page", defaultValue = "0") String pageNumber,
                                                    @RequestParam(name = "sort", defaultValue = Sort.TREND) String sort,
                                                    @PathVariable("year") String year) {
         return videoTitlesService.getVideoTitles(pageNumber, sort, Req.SERIALS_URL + "/show_start/"+year);
